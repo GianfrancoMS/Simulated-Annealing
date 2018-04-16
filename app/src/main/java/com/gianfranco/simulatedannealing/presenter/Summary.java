84c16c6b5c518f13270eae60272ce17d7f52484f
GianfrancoMS <gianfrancomonsal@gmail.com>
2018-04-15 23:09:11 -0500

    Add Summary class for holding data when optimization ends

---
+++ app/src/main/java/com/gianfranco/simulatedannealing/presenter/Summary.java
@@ -0,0 +1,33 @@
+package com.gianfranco.simulatedannealing.presenter;
+
+import com.gianfranco.simulatedannealing.model.Result;
+
+public final class Summary {
+    private String iterations;
+    private String replacements;
+    private String temperature;
+    private String distance;
+
+    public void setResult(Result result) {
+        this.iterations = Integer.toString(result.iterations());
+        this.replacements = Integer.toString(result.replacements());
+        this.temperature = Double.toString(result.temperature());
+        this.distance = Double.toString(result.distance());
+    }
+
+    public String iterations() {
+        return iterations;
+    }
+
+    public String replacements() {
+        return replacements;
+    }
+
+    public String temperature() {
+        return temperature;
+    }
+
+    public String distance() {
+        return distance;
+    }
+}
